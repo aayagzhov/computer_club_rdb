996061c193d7c59475d6dab1b04829ab9e904989
Alexey Yagzhov <aayagzhov@edu.hse.ru>
2025-12-16 19:42:02 +0300

    add seed

--- scripts/seed_data.bat
+++ scripts/seed_data.bat
@@ -8,3 +8,10 @@ docker exec central_db bash -c "psql -U admin -d computer_club_rdb -f /seed/1_in
 echo.
 echo === Waiting for all nodes to be ready (5 seconds) ===
 timeout /t 5 /nobreak > nul
+
+set CONTAINERS=central_db club1_db club2_db club3_db
+
+for %%C in (%CONTAINERS%) do (
+    echo === Setting up %%C ===
+    docker exec %%C bash -c "psql -U admin  -d computer_club_rdb -f /seed/2_seed_data.sql"
+)
